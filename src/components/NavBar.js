378d762032acba1344e2db24bc9701c8e967ed8a
Aarow Epstein <aarowepstein1@gmail.com>
2023-02-28 20:03:09 -0700

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const postLinks = links.map((link) => {
+    
+    return (<a key={link} href={"#" + link}>{link}</a>)
+  })
+  return <nav>{postLinks}</nav>;
 }
 
 export default NavBar;
